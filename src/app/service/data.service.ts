57cf77c177e78a7a1c2974217aec807441033b43
Tobias Wright <tobias.wright@gmail.com>
2024-08-27 22:28:12 -0500

    updated data service

--- src/app/service/data.service.ts
+++ src/app/service/data.service.ts
@@ -1,9 +1,38 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
+
+import data from '../assets/data.json'
+
+interface ColorData {
+  cause:string,
+  causeFull:string,
+  htmlcolor:string
+}
 
 @Injectable({
   providedIn: 'root'
 })
-export class DataService {
+export class DataService implements OnInit {
+  private data = data;
+  private sortedData:ColorData[] = [];
 
   constructor() { }
+
+   ngOnInit(): void {
+     this.sortedData = this.sortList( this.data, 'cause');
+   }
+
+  get colorData() {
+    return this.sortedData
+  }
+
+  private sortList( arr: ColorData[], prop: string) {
+    return arr.sort( (a, b) => {
+      const c = prop as keyof ColorData;
+
+      const x = a[c].toLowerCase();
+      const y = b[c].toLowerCase();
+      return x.localeCompare(y)
+    })
+  }
+
 }
