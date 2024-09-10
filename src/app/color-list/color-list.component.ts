a24c23a1167aff64bba9ac2f76d7cb9899255a02
Tobias Wright <tobias.wright@gmail.com>
2024-09-10 08:30:32 -0500

    updated data service

--- src/app/color-list/color-list.component.ts
+++ src/app/color-list/color-list.component.ts
@@ -1,11 +1,7 @@
-import { Component, effect, inject, OnInit } from '@angular/core';
+import { Component, computed, effect, inject, signal } from '@angular/core';
 import { DataService } from '../service/data.service';
 
-interface ColorData {
-  cause:string,
-  causeFull:string,
-  htmlcolor:string
-}
+import { type Data } from '../color-data.model';
 
 @Component({
   selector: 'app-color-list',
@@ -15,26 +11,52 @@ interface ColorData {
   styleUrl: './color-list.component.css'
 })
 export class ColorListComponent {
-  DataService = inject( DataService );
-  list = this.DataService.getColorData();
+  causeData = inject( DataService ).getCauseData();
+  isSet = true;
+  isReversed = signal(false);
+  filterColor = signal<string | null>(null);
+  causeList = computed(() => {
+    let data = this.causeData();
+    if (this.filterColor()) {
+      data = this.causeData().filter(
+        (item) => item.colorData.htmlcolor[0] === this.filterColor()
+      );
+    }
+
+    if (this.isReversed()) {
+      return this.sortList(data, this.isReversed());
+    }
+
+    return this.sortList(data);
+  })
   
   constructor() {
-    // console.log("allo")
     effect(() => {
-      console.log("all", this.list());
+      console.log("allo", this.causeData());
     });
-
-
-
-    // this.list.
   }
 
-  onButtonTest() {
-    this.DataService.sortData();
+  reverseList() {
+    this.isReversed.set(!this.isReversed());
   }
 
+  filterByColor() {
+    let toggle = this.isSet ? 'red' : null;
+    this.filterColor.set(toggle);
+    this.isSet = !this.isSet
+  }
 
-  // OnInit() {
-  //   console.log("allo", this.data.colorData)
-  // }
+  private sortList(arr: Data[], reverse = false, prop = 'cause') {
+    return arr.sort((a: Data, b: Data) => {
+      const c = prop as keyof Data;
+
+      const x = b[c] as string;
+      const y = a[c] as string
+      
+      if (reverse) {
+        return x.localeCompare(y);
+      }
+      return y.localeCompare(x);
+    });
+  }
 }
--- src/app/service/data.service.ts
+++ src/app/service/data.service.ts
@@ -1,23 +1,23 @@
 import { Injectable, signal } from '@angular/core';
- 
-import data from '../assets/data.json'
+
+import data from '../assets/data.json';
 import colorMap from '../assets/color-map.json';
 import { type Data } from '../color-data.model';
 
 interface ColorData {
-  cause:string,
-  causeFull:string,
-  htmlcolor:string
+  cause: string;
+  causeFull: string;
+  htmlcolor: string;
 }
 
 interface ColorMap {
-  name:string,
-  displayName:string,
-  hexCode:string
+  name: string;
+  displayName: string;
+  hexCode: string;
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
   private unsortedData: ColorData[] = data;
@@ -25,38 +25,34 @@ export class DataService {
   data = signal<Data[]>([]);
 
   constructor() {
-    let sortedList = this.sortList( this.unsortedData, 'cause' );
-    let map = this.createMap( this.unsortedColorMap );
+    let map = this.createMap(this.unsortedColorMap);
+    let causeObj = this.createCauseObj(this.unsortedData, map);
 
-    this.data.set(this.addColorMap(sortedList, map));
+    this.data.set(causeObj);
   }
 
-  getColorData() {
-    return this.data
-  }
-
-  sortData() {
-    // this.unsortedData.update( ()=> this.sortList( this.unsortedData(), 'cause') )
-      // this.unsortedData.set( data )
-      // console.log( this.unsortedData())
+  getCauseData() {
+    return this.data;
   }
 
   private createMap(array: ColorMap[]) {
     let map = new Map();
-    array.forEach(item => {
-      map.set(item.name, {...item});
-    })
-    return map
+    array.forEach((item) => {
+      map.set(item.name, { ...item });
+    });
+    return map;
   }
 
-  private addColorMap( arr: ColorData[], map: Map<string, ColorMap>  ):Data[] {
-    return arr.map( (item) => {
+  private createCauseObj(arr: ColorData[], map: Map<string, ColorMap>): Data[] {
+    return arr.map((item) => {
       const htmlcolor = item.htmlcolor.split(',');
-      const causeFull = item.causeFull !== "" ? item.causeFull : item.cause;
+      const causeFull = item.causeFull !== '' ? item.causeFull : item.cause;
       const isSingle = htmlcolor.length > 1 ? false : true;
       const id = Math.random() * 100;
-      const displayName = htmlcolor.map( item => map.get(item)!.displayName).join(", ");
-      const hexCode = htmlcolor.map( item => map.get(item)!.hexCode);
+      const displayName = htmlcolor
+        .map((item) => map.get(item)!.displayName)
+        .join(', ');
+      const hexCode = htmlcolor.map((item) => map.get(item)!.hexCode);
 
       return {
         cause: item.cause,
@@ -66,20 +62,9 @@ export class DataService {
         colorData: {
           htmlcolor: htmlcolor,
           displayName: displayName,
-          hexCode: hexCode
-        }
-      }
-    })
+          hexCode: hexCode,
+        },
+      };
+    });
   }
-
-  private sortList( arr: ColorData[], prop: string) {
-    return arr.sort( (a:ColorData, b:ColorData) => {
-      const c = prop as keyof ColorData;
-
-      const x = a[c].toLowerCase();
-      const y = b[c].toLowerCase();
-      return x.localeCompare(y)
-    })
-  }
-
 }
