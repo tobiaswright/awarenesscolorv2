fe19eb0ec7186b3ed4ab7e0fa5efad637cd0e4f2
Tobias Wright <tobias.wright@gmail.com>
2025-02-04 20:57:44 -0600

    Auto-generated commit

--- src/app/truncate-names.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TruncateNamesPipe } from './truncate-names.pipe';
-
-describe('TruncateNamesPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TruncateNamesPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
